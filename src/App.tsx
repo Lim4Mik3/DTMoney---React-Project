b5dabc07ba393500cbe8bde8e30ed104e3196a40
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-23 16:58:13 -0300

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export { App };
