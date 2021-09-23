4e6894e4c8e380964002a38b84f33699e4168629
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-23 17:37:02 -0300

    Configuring responsiveness and external source

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,9 +1,19 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
-  :root {
+  :root{
     --background: #f0f2f5;
-  }
+    --red: #E52e54;
+    --blue: #5429CC;
+    --green: #33CC95;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+
+    --shape: #FFFFFF
+}
   
   * {
     margin: 0;
@@ -11,9 +21,37 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 
 `;
