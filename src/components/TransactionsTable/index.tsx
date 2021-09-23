5964de2a71775dc33348cf36301e120e3ab06509
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-23 20:14:51 -0300

    Created transactions table and their styled

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/08/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 800,00</td>
+            <td>Casa</td>
+            <td>19/09/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
