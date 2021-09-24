fc461aa4382d4691f1929412537c6b041089aa80
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-23 23:00:42 -0300

    Modal component created and set up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles';
 
-function Header() {
+import logoImg from '../../assets/logo.svg'
+
+interface HeaderProps {
+  onNewTransactionModalOpen: () => void;
+}
+
+function Header({ onNewTransactionModalOpen }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="DT Money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onNewTransactionModalOpen}>Nova transação</button>
       </Content>
     </Container>
   )
