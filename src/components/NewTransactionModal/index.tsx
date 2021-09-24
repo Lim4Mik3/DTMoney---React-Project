4a75da3b4609214226cd95dffc19c3ddf00ffc32
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-09-24 01:18:18 -0300

    Creating transaction type button and their custom style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react'
 import Modal from 'react-modal'
+
+import { Container, ContainerTransactionType, RadioBox } from './styles'
+
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -6,12 +13,62 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Nova transação</h2>
+      <button 
+        type="button" 
+        className="react-modal-close"
+        onClick={onRequestClose}
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <Container>
+        <h2>Nova transação</h2>
+
+        <input 
+          placeholder="Titulo"
+        />
+
+        <input 
+          type="number"
+          placeholder="Preço"
+        />
+
+        <ContainerTransactionType>
+          <RadioBox 
+            type="button"
+            onClick={() => { setType('deposit') }}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => { setType('withdraw') }}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </RadioBox>
+        </ContainerTransactionType>
+
+        <input 
+          placeholder="Categoria"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,8 +4,8 @@ export const GlobalStyles = createGlobalStyle`
   :root{
     --background: #f0f2f5;
     --red: #E52e54;
-    --blue: #5429CC;
     --green: #33CC95;
+    --blue: #5429CC;
 
     --blue-light: #6933FF;
 
@@ -52,6 +52,43 @@ export const GlobalStyles = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background-color: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.7);
+    }
   }
 
 `;
